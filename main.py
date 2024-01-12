9484dbe875014e02d4e642503e9c73808ad65d41
Leon <l.beccard@protonmail.com>
2024-01-12 17:08:29 +0100

    Add text extraction methods

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import traceback
 import fitz
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
+from utils import requests
+from utils.text_parser import create_meta_information, get_raw_text
 from utils.utils import PluginUtility
 
 app = FastAPI()
@@ -54,6 +56,29 @@ def upload(file: UploadFile = File(...)):
                     page, index, path_to_new_directory
                 )
                 categories.append(category)
+
+                # Try extracting raw text from page with exception handling
+                try:
+                    raw_text = get_raw_text(page)
+                except IOError as e:
+                    traceback.print_exc()
+                    error_message = f"Error extracting: {e}"
+                    raise IOError(error_message) from e
+
+                # Try posting raw text and category to Wordpress backend with exception handling
+                try:
+                    meta_information = create_meta_information(category)
+                    response = requests.upload_post(meta_information, raw_text, "")
+                except IOError as e:
+                    traceback.print_exc()
+                    error_message = f"Error posting: {e}"
+                    raise ValueError(error_message) from e
+
+                if response.status_code != 200 and response.status_code != 201:
+                    raise IOError(
+                        f"Error posting to Wordpress: {response.content} with status code {response.status_code}"
+                    )
+
                 # DTodo: create post with type papers and the name of the issue # noqa: E501
                 # DTodo: create new term in category "papers" with the name of the issue # noqa: E501
                 # DTodo: create new keycloak role with the name of the issue
